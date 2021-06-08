d955bc81683866af651accc7a45b4a5917d26344
Takehiro Hagiwara <hagiwara.takehiro@classmethod.jp>
2021-06-08 16:40:20 +0900

    until 5.3

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -34,13 +34,20 @@ trait Stream[+A] {
 
   def take(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
-    case Cons(h, _) => cons(h(), empty)
+    case Cons(h, _) if n == 1 => cons(h(), empty)
     case _ => empty
   }
 
-  def drop(n: Int): Stream[A] = ???
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
 
   def forAll(p: A => Boolean): Boolean = ???
 
--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -7,12 +7,12 @@ class StreamSpec extends FreeSpec with Matchers {
 
   "5.1" - {
     "use foldRight" in {
-      cons({1}, cons({2}, Stream.empty)).toList shouldBe List(1, 2)
+      cons(1, cons(2, Stream.empty)).toList shouldBe List(1, 2)
       Stream().toList shouldBe List.empty
     }
 
     "use recursive" in {
-      cons({1}, cons({2}, Stream.empty)).toList2 shouldBe List(1, 2)
+      cons(1, cons(2, Stream.empty)).toList2 shouldBe List(1, 2)
       Stream().toList2 shouldBe List.empty
     }
   }
@@ -20,23 +20,46 @@ class StreamSpec extends FreeSpec with Matchers {
   "5.2" - {
     "take" - {
       "take less than length" in {
-        val taken = Stream({1}, {2}, {3}).take(2)
+        val taken = Stream(1, 2, 3).take(2)
         taken.toList shouldBe List(1, 2)
       }
 
       "take all" in {
-        val taken = Stream({1}, {2}, {3}).take(3)
+        val taken = Stream(1, 2, 3).take(3)
         taken.toList shouldBe List(1, 2, 3)
       }
 
       "take over" in {
-        val taken = Stream({1}, {2}, {3}).take(4)
+        val taken = Stream(1, 2, 3).take(4)
         taken.toList shouldBe List(1, 2, 3)
 
         // def func(x: => A)
         // func(h()) // h() is not executed. laziness.
+        // func({print("test")}) // print is not executed. laziness.
       }
     }
+
+    "drop" - {
+      "drop less than length" in {
+        val dropped = Stream(1, 2, 3).drop(1)
+        dropped.toList shouldBe List(2, 3)
+      }
+
+      "drop all" in {
+        val dropped = Stream(1, 2, 3).drop(3)
+        dropped.toList shouldBe List()
+      }
+
+      "drop over" in {
+        val dropped = Stream(1, 2, 3).drop(4)
+        dropped.toList shouldBe List()
+      }
+    }
+  }
+
+  "5.3" in {
+    val taken = Stream(1, 2, 3, 4).takeWhile(_ < 3)
+    taken.toList shouldBe List(1, 2)
   }
 
 }
